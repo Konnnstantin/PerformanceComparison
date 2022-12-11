96c58d6783258a944bd910f084269b53239330ec
Konstantin <kurakin-1993@mail.ru>
2022-12-11 14:18:21 +0400

    Add project files.

---
+++ PerfomanceCompairLinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+{
+    var path = "C:\\Users\\user\\Desktop\\Text1.txt";
+    StreamReader sr = new StreamReader(path);
+    string text = sr.ReadToEnd();
+    var watchTwo = Stopwatch.StartNew();
+
+    LinkedList<string> strings = new LinkedList<string>();
+    strings.AddFirst(text);
+    foreach (var i in strings)
+    {
+        Console.WriteLine(i);
+    }
+    Console.WriteLine($"Вставка в  словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+}
---
+++ PerformanceComparison/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+{
+    var path = "C:\\Users\\user\\Desktop\\Text1.txt";
+    StreamReader sr = new StreamReader(path);
+    string text = sr.ReadToEnd();
+
+    var watchTwo = Stopwatch.StartNew();
+
+    List<string> list = new List<string>();
+    list.Add(text);
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine($"Вставка в  словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+}
